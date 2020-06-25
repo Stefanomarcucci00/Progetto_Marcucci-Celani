e73b5cd1aac7ae51a7616dcdc5671d994c2d426e
alessandro49 <66130406+alessandro49@users.noreply.github.com>
2020-06-25 21:23:57 +0200

    Add files via upload
    Consegna progetto

--- PROGETTO PROGRAMMAZIONE A OGGETTI/Progetto_Marcucci-Celani-master/PROGETTO PROGAMMAZIONE A OGGETTI/Project/src/main/java/it/univpm/Project/ProjectApplication.java
+++ PROGETTO PROGRAMMAZIONE A OGGETTI/Progetto_Marcucci-Celani-master/PROGETTO PROGAMMAZIONE A OGGETTI/Project/src/main/java/it/univpm/Project/ProjectApplication.java
@@ -1,12 +1,14 @@
 package it.univpm.Project;
 
+import java.io.IOException;
+import org.apache.tomcat.util.json.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException, IOException {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
